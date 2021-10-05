db6d0d73ca2fa66bc66ad743cdb48e24a9d4b311
jhs512 <jangka512@gmail.com>
2021-10-05 12:32:10 +0900

    안드로이드 폰에서 라이브리로드

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from 'react'
+import { Toast } from '@capacitor/toast';
+import { Device } from '@capacitor/device';
+
+
+const showHelloToast = async () => {
+  await Toast.show({
+    text: 'Hello!!!!!!',
+  });
+};
+
+const logBatteryInfo = async () => {
+  const info = await Device.getBatteryInfo();
+
+  await Toast.show({
+    text: JSON.stringify(info),
+  });
+};
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div className="w-[100px] h-[100px] bg-[#afafaf]"></div>
+      <div onClick={logBatteryInfo} className="w-[50px] h-[150px] bg-[#afafaf]"></div>
     </div>
   )
 }
